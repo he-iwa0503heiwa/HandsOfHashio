08e5087bcd72960d5ac5a3f4ee7dbada3b20ffc1
heiwa <heiwa.2012.nin@gmail.com>
2017-01-27 19:45:30 +0900

    Initial Commit

---
+++ HandsOfHashio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandsOfHashio
+//
+//  Created by heiwa on 2017/01/27.
+//  Copyright © 2017年 heiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
